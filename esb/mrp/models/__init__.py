06c1484c10bd0f241374f914b8c2b69cce8a5bc6
ps-tubtim <pimolnats@ecosoft.co.th>
2020-04-29 22:51:51 +0700

    [13.0][ADD] ESB: mrp

--- esb/mrp/models/__init__.py
+++ esb/mrp/models/__init__.py
@@ -1,5 +1,3 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
-from . import department
-from . import mrp
-from . import product
+from . import mrp_production
---
+++ esb/mrp/models/mrp_production.py
@@ -0,0 +1,25 @@
+# Copyright 2020 Ecosoft Co., Ltd (http://ecosoft.co.th/)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+from odoo import models, fields
+
+
+class MrpProduction(models.Model):
+    _inherit = "mrp.production"
+
+    machine_number = fields.Char(string="Machine Number", size=30)
+    operator = fields.Char(string="Operator", size=30)
+    shift = fields.Selection(
+        selection=[("day", "Day"), ("night", "Night")],
+        string="Shift",
+        default="day",
+    )
+    assembly_line = fields.Selection(
+        selection=[("auto", "Auto"), ("manual", "Manual"), ("line3", "Line 3")],
+        string="Assembly Line",
+        default="auto",
+    )
+    delay = fields.Boolean(string="Delay")
+    remark = fields.Text(string="Remark", translate=True)
+    net_weight = fields.Float(string="Net Weight(kg/pcs)", default=0.00)
+    gross_weight = fields.Float(string="Net Weight + Inner Box(kg/pcs)", default=0.00)
