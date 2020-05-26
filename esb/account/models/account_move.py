2d291f0669e9434c1ce8513724dc898ccaa5cd90
ps-tubtim <pimolnats@ecosoft.co.th>
2020-05-26 15:03:25 +0700

    [13.0][UPD] ESB: Form, discount

--- esb/account/models/account_move.py
+++ esb/account/models/account_move.py
@@ -11,6 +11,41 @@ class AccountMove(models.Model):
         comodel_name="esb.packing.list",
         string="Packing List",
     )
+    discount_last = fields.Float(
+        string="Discount (%)",
+        digits="Discount",
+        default=0.0,
+    )
+    discount_special = fields.Float(
+        string="Special Discount (%)",
+        digits="Discount",
+        default=0.0,
+    )
+
+    _sql_constraints = [
+        (
+            "discount_last_limit",
+            "CHECK (discount_last <= 100.0)",
+            "Discount must be lower than 100%.",
+        ),
+        (
+            "discount_special_limit",
+            "CHECK (discount_special <= 100.0)",
+            "Discount must be lower than 100%.",
+        ),
+    ]
+
+    @api.onchange("discount_last", "discount_special")
+    def _onchange_discount_last(self):
+        # Change discount_last to discount3
+        # Change discount_special to discount2
+        for record in self:
+            for line in record.invoice_line_ids:
+                line.discount3 = record.discount_last
+                line.discount2 = record.discount_special
+                line._onchange_triple_discount()
+                line._onchange_price_subtotal()
+                line._onchange_balance()
 
     @api.depends("partner_id", "company_id")
     def _compute_sale_type_id(self):
@@ -19,3 +54,20 @@ class AccountMove(models.Model):
         for record in self:
             if record.type not in ["out_invoice", "out_refund"]:
                 record.sale_type_id = False
+
+
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+
+    subtotal_no_disc = fields.Monetary(
+        string="Subtotal",
+        store=True,
+        compute="_compute_subtotal_no_disc",
+        currency_field="always_set_currency_id",
+        help="Subtotal not including discount",
+    )
+
+    @api.depends("quantity", "price_unit")
+    def _compute_subtotal_no_disc(self):
+        for record in self:
+            record.subtotal_no_disc = record.quantity * record.price_unit
